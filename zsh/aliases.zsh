59fbb135d732edd117e212914ea63fd0f62d4c7b
Laurence Willetts <laurencewilletts@protonmail.com>
2025-12-20 13:05:16 +0000

    zsh alias and prompt changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -21,6 +21,8 @@ alias n="niri-session"
 alias x="startx"
 # alias x="xinit -- :0 vt2 2> /dev/null &!"
 alias vol="pulsemixer"
+alias mus="jellyfin-tui"
+alias pick="hyprpicker"
 alias htop="htop -t -s PERCENT_CPU"
 alias grep="grep --color=auto"
 alias diff="diff --color=auto"
@@ -28,24 +30,25 @@ alias todo="$EDITOR $HOME/usr/notes/todo.md"
 alias clock="xclock -d -face "Cozette-8" -padding 8 -update 5 2>/dev/null &!"
 alias ffmpeg="ffmpeg -hide_banner"
 alias ffprobe="ffprobe -hide_banner"
-alias glances="glances --hide-kernel-threads"
-
-alias pick="niri msg pick-color"
 
 # net
 alias net="curl -4 icanhazip.com"
 alias wget="wget --no-hsts"
 
+# ansible
+alias pl="ansible-playbook -K"
+alias role="ansible-role"
+alias vault="ansible-vault"
+alias ve="ansible-vault edit"
+
 # oneliners
 alias coin="curl -s rate.sx"
 alias btc="curl -s rate.sx/btc"
-alias plot="systemd-analyze plot > boot.svg && mpv boot.svg"
 alias latency="sudo tc qdisc add dev enp10s0u1c2 root netem delay 72ms"
 alias reseteth="sudo tc qdisc del dev enp10s0u1c2 root"
 alias stress="stress-ng --cpu 16 --timeout 60s --metrics-brief"
 
 # file handling
-alias i="img2sixel"
 alias p="zathura"
 alias q="qutebrowser"
 alias strip="jhead -de"
@@ -55,6 +58,13 @@ alias findexec="fd . --exact-depth 1 -t x"
 alias irc="dtach -A /tmp/irc weechat"
 alias ts="systemd-run --user --scope tmux new-session"
 
+# systemd
+alias j="journalctl"
+alias ctl="systemctl"
+alias user="systemctl --user"
+alias failed="systemctl --failed"
+alias resetfailed="sudo systemctl reset-failed" 
+alias plot="systemd-analyze plot > boot.svg && swayimg boot.svg"
 alias modcat="systemd-analyze cat-config modprobe.d"
 
 # ascii
@@ -62,7 +72,6 @@ alias unix="printf '%s\n' \"\$(curl -sL git.io/unix)\""
 alias taco="printf '%s\n' \"\$(curl -sL git.io/taco)\""
 
 # misc
-alias snake="terminibbles -d 3 -q"
 alias engage="play -c2 -n synth whitenoise band -n 100 24 band -n 300 100 \
 gain +4 synth whitenoise lowpass -1 100 lowpass -1 100 lowpass -1 100 gain +2"
 alias machine="clear; lstopo-no-graphics -.ascii; printf '\n'"
