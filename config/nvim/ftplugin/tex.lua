b274973647bdc2f138a9a457cda8283fb7b305c2
Laurence Willetts <laurencewilletts@protonmail.com>
2026-09-15 19:23:59 +0100

    fix: nvim colour scheme
    also add gitcommit ftplugin detection

--- config/nvim/ftplugin/tex.lua
+++ config/nvim/ftplugin/tex.lua
@@ -2,7 +2,7 @@ vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
 vim.opt_local.spell = true
 vim.opt_local.textwidth = 80
-vim.opt_local.conceallevel = 1
+vim.opt_local.conceallevel = 0
 vim.opt_local.concealcursor = "nc"
 vim.opt_local.formatoptions:append("t") -- auto-wrap text
 vim.opt_local.formatoptions:append("n") -- numbered lists
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -41,7 +41,7 @@ vim.opt.laststatus = 0
 -- terminal colors
 vim.opt.background = 'light'
 vim.opt.termguicolors = false
-vim.cmd.colorscheme = 'morning'
+vim.cmd([[colorscheme shine]])
 
 -- vim.g.terminal_color1 = '#1C1C1C'
 -- vim.g.terminal_color2 = '#CA3448'
@@ -52,9 +52,9 @@ vim.cmd.colorscheme = 'morning'
 -- vim.g.terminal_color6 = '#9FB9A8'
 -- vim.g.terminal_color7 = '#D9EAE2'
 
-vim.api.nvim_set_hl(0, 'Normal', { fg = "#D9EAE2", bg="#1C1C1C" })
-vim.api.nvim_set_hl(0, 'Visual', { fg = "#1C1C1C", bg="#D9EAE2" })
-vim.api.nvim_set_hl(0, 'Comment', { fg = "#9FB9A8", bg= "#1C1C1C" })
+-- vim.api.nvim_set_hl(0, 'Normal', { fg = "#D9EAE2", bg="#1C1C1C" })
+-- vim.api.nvim_set_hl(0, 'Visual', { fg = "#1C1C1C", bg="#D9EAE2" })
+-- vim.api.nvim_set_hl(0, 'Comment', { fg = "#9FB9A8", bg= "#1C1C1C" })
 
 -- cursor
 vim.opt.guicursor = 'n:blinkon0'
@@ -99,7 +99,17 @@ vim.opt.splitright = true
 
 -- autocmds
 local autocmd = vim.api.nvim_create_autocmd
+
 autocmd('BufWritePost', {pattern = 'init.lua', command = 'source $MYVIMRC'})
+
+autocmd('BufLeave', {
+    pattern = "*.tex",
+    callback = function()
+        if vim.b.vimtex then
+            vim.cmd("VimtexClean")
+        end
+    end,
+})
 
 -- key remaps
 vim.g.mapleader = " "
