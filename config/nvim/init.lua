6688b476d890ac0441001f907aefecbc6bb04630
Laurence Willetts <laurencewilletts@protonmail.com>
2026-09-18 18:32:33 +0100

    chore: update nvim colors

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -160,7 +160,7 @@ vim.opt.showmode = true
 vim.opt.laststatus = 0
 
 -- terminal colors
-vim.opt.background = 'dark'
+vim.opt.background = 'light'
 vim.opt.termguicolors = true
 
 -- vim.g.terminal_color1 = '#1C1C1C'
@@ -172,9 +172,9 @@ vim.opt.termguicolors = true
 -- vim.g.terminal_color6 = '#9FB9A8'
 -- vim.g.terminal_color7 = '#D9EAE2'
 
-vim.api.nvim_set_hl(0, 'Normal', { fg = "#D9EAE2", bg="#1C1C1C" })
-vim.api.nvim_set_hl(0, 'Visual', { fg = "#1C1C1C", bg="#D9EAE2" })
-vim.api.nvim_set_hl(0, 'Comment', { fg = "#9FB9A8", bg= "#1C1C1C" })
+-- vim.api.nvim_set_hl(0, 'Normal', { fg = "#D9EAE2", bg="#1C1C1C" })
+-- vim.api.nvim_set_hl(0, 'Visual', { fg = "#1C1C1C", bg="#D9EAE2" })
+-- vim.api.nvim_set_hl(0, 'Comment', { fg = "#9FB9A8", bg= "#1C1C1C" })
 
 -- cursor
 vim.opt.guicursor = 'n:blinkon0'
